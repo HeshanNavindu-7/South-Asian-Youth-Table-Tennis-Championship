d0bf1942b913affb772d8c981292de8702bf411e
heshan <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-02-08 15:38:12 +0530

    card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import "./card.css"
 const Card = () => {
   return (
-    <div>
+    <div className='text-center'>
+        <h1 className="text-4xl font-bold mt-24 ">FIXTURES & RESULTS</h1>
+    
+          <div className='flex justify-center mt-16  '>
+       
+            <div className='card bg-blue-700 text-white w-5/12   shadow-md px-8 py-6'>
 
+            </div>    
+            <div className='card bg-red-800 text-white w-5/12  shadow-md px-8 py-6'>
+
+            </div>    
+         </div>
     </div>
   )
 }
