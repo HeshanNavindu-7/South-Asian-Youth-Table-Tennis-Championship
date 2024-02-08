335e1c45f4e460d61617635d0239dbacccfd17f8
heshan <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-02-08 19:27:03 +0530

    contact us

---
+++ src/components/ContactUs/Contact.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import './contact.scss'
+const Contact = () => {
+  return (
+    <div>
+       <div className='text-white m-5 text-center overflow-hidden'>
+    <h1 className='text-4xl font-bold mt-12 text-center overflow-hidden'>CONTACT US</h1>
+		<div className="contact-us ">
+     
+			<div className="contact-card">
+				<div className="profile-icon">
+					<img src="/images/Landingpage/1(1).png" alt="" />
+				</div>
+				<div className="position">
+					<h1>ISHARA DABARERA</h1>
+					<p>WIE Chair</p>
+				</div>
+				<div className="social-media">
+					<div>
+						<img src="/images/Landingpage/phone.png" alt="" />
+						<span>+9478467229</span>
+					</div>
+					<div>
+						<img src="/images/Landingpage/linkedin.png" alt="" />
+						<span><a href="https://www.linkedin.com/in/ishara-dabarera-476315246/" target="_blank">Ishara Dabarera</a></span>
+					</div>
+				</div>
+				<div className="email">
+					<img src="/images/Landingpage/mail.png" alt="" />
+					<span>isharaecc1998@gmail.com</span>
+				</div>
+				<div className="back"></div>
+			</div>
+			<div className="contact-card">
+				<div className="profile-icon">
+					<img src="/images/Landingpage/2 (2).png" alt="" />
+				</div>
+				<div className="position">
+					<h1>THARUSHI WIJETHUNGA</h1>
+					<p>WIE Vice Chair</p>
+				</div>
+				<div className="social-media">
+					<div>
+						<img src="/images/Landingpage/phone.png" alt="" />
+						<span>+94763089219</span>
+					</div>
+					<div>
+						<img src="/images/Landingpage/linkedin.png" alt="" />
+						<span><a href="https://www.linkedin.com/in/tgrw99/" target="_blank">Tharushi Wijethunga</a></span>
+					</div>
+				</div>
+				<div className="email">
+					<img src="/images/Landingpage/mail.png" alt="" />
+					<span>tharushiwijethunga999@gmail.com</span>
+				</div>
+				<div className="back"></div>
+			</div>
+			<div className="contact-card">
+				<div className="profile-icon">
+					<img src="/images/Landingpage/3.png" alt="" />
+				</div>
+				<div className="position">
+					<h1>ZULFA ZULFICKAR</h1>
+					<p>Project Chair</p>
+				</div>
+				<div className="social-media">
+					<div>
+						<img src="/images/Landingpage/phone.png" alt="" />
+						<span>+94769188841</span>
+					</div>
+					<div>
+						<img src="/images/Landingpage/linkedin.png" alt="" />
+						<span><a target="_blank" href="https://www.linkedin.com/in/zulfa-zulfikar-5a643521a/">Zulfa Zulfikar</a></span>
+					</div>
+				</div>
+				<div className="email">
+					<img src="/images/Landingpage/mail.png" alt="" />
+					<span>sulfafathima11@gmail.com</span>
+				</div>
+				<div className="back"></div>
+			</div>
+			<div className="contact-card">
+				<div className="profile-icon">
+					<img src="/images/Landingpage/4.png" alt="" />
+				</div>
+				<div className="position">
+					<h1>MINURI SENARA</h1>
+					<p>Designing & Content Co-Chair</p>
+				</div>
+				<div className="social-media">
+					<div>
+						<img src="/images/Landingpage/phone.png" alt="" />
+						<span>+94787531133</span>
+					</div>
+					<div>
+						<img src="/images/Landingpage/linkedin.png" alt="" />
+						<span><a href="https://www.linkedin.com/in/minuri-hewage-20b537235/" target="_blank">Minuri Hewage</a></span>
+					</div>
+				</div>
+				<div className="email">
+					<img src="/images/Landingpage/mail.png" alt="" />
+					<span>mshewage@std.appsc.sab.ac.lk</span>
+				</div>
+				<div className="back"></div>
+			</div>
+		</div>
+		</div>
+    </div>
+  )
+}
+
+export default Contact
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import CountdownComponent from '../../components/Countdown/Countdown';
 import Participant from './components/Participant/Participant';
 import Card from '../../components/Card/Card';
+import Contact from '../../components/ContactUs/Contact';
 
 const HomePage = () => {
   return (
@@ -12,6 +13,7 @@ const HomePage = () => {
      <CountdownComponent/>
     <Participant/>
     <Card/>
+    <Contact/>
     </div>
   );
 };
