818e9bcc9fe822062e0ec32a4da7b3462ed62e56
heshan <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-02-22 01:56:05 +0530

    news

--- src/pages/NewsPage/components/News.jsx
+++ src/pages/NewsPage/components/News.jsx
@@ -1,31 +1,43 @@
-// src/components/Header.jsx
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { Mail, User, Inbox, Menu, Delete } from 'react-feather';
-import { BsChatLeftText,BsFacebook } from "react-icons/bs";
+import React from 'react';
 import './News.css';
 
 const News = () => {
-  
-
   return (
-
     <div>
-         <div className="about1 m-auto w-full h-52 overflow-hidden">
+      <div className="about1 text-center m-auto w-full h-52 overflow-hidden">
         <h2 className="text-center text-6xl relative top-20 text-white">News</h2>
       </div>
-      
-      <div className="dis w-full mt-10">
-        <h2 className="text-center w-1/2 m-auto text-3xl">Tennis is a racket sport that is played either individually or between two teams of two players each. Each player uses a tennis racket that is strung with cord to strike a hollow rubber ball covered with felt over or around a net and into the opponent's court. </h2>
+      <div className="dis w-full text-center mt-10">
+        <h2 className="text-center w-full sm:w-1/2 m-auto font-semibold text-3xl">
+          Press Conference Announcement
+        </h2>
+      </div>
+
+      <div className="cards flex flex-col sm:flex-row justify-center text-center mt-10">
+        <div className="box lg:w-56 mb-4 mx-auto">
+          <img
+            src="./images/news1.jpeg"
+            className="custom-shape  lg:max-w-80 lg:mx-1"
+            alt=""
+          />
+        </div>
+        <div className="box  md:text-center lg:w-1/3 mb-4 lg:ml-12">
+          <div className="lg:text-left text-center">
+            <h3>
+              TTASL is thrilled to announce the upcoming press conference for the
+              highly anticipated South Asian Youth Table Tennis Championship 2024.
+            </h3>
+            <h3 className='font-semibold text-xl'>Date </h3>
+            <p>February 22, 2024 </p>
+
+            <h3 className='font-semibold text-xl'>Time </h3>
+            <p>5:00 pm onwards </p>
+            <h3 className='font-semibold text-xl'>Location</h3>
+            <p>University of Colombo</p>
+          </div>
+        </div>
       </div>
     </div>
-    
-
-
-   
-        
-        
-        
   );
 };
 
