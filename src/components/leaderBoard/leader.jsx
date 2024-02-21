0e008a40e30a00143ea815c8b9a2d9fcb7906ade
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-02-22 01:47:59 +0530

    newUpdate

--- src/components/leaderBoard/leader.jsx
+++ src/components/leaderBoard/leader.jsx
@@ -1,11 +1,82 @@
 // src/components/Leader.js
 import React, { useState, useEffect } from 'react';
+import './leader.css';
  // Adjust the path as per your project structure
 
 const LEADER=()=>{
   return (
     <div>
-      <h1>Leaders</h1>
+      <div className="mcontainer1">
+        <h2>Table Tennis, also known as ping-pong, is a fast-paced indoor sport that has gained immense popularity worldwide due to its accessibility, simplicity, and exhilarating gameplay. It is played both recreationally and competitively, with millions of enthusiasts participating in clubs, leagues, and international tournaments.</h2>
+      </div>
+      <div className="mcontainer2">
+        <div className="dis">
+          <h2>origins</h2>
+          <p>Table Tennis originated in England during the late 19th century as a parlor game, often played by the upper-class society as a miniature version of lawn tennis. It evolved from various lawn tennis adaptations played on various surfaces indoors.</p>
+        </div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Ping-Pong_2.jpg" width="700px" alt="" />
+      </div>
+      <div className="mcontainer3 mcontainer2">
+        
+        <img src="https://graphics8.nytimes.com/images/2013/09/22/magazine/pingpong-look-slide-5J6F/pingpong-look-slide-5J6F-jumbo-v2.jpg" width="700px" alt="" />
+        <div className="dis">
+          <h2>Evolution</h2>
+          <p> The game saw significant development over the years, with the introduction of celluloid balls in the late 19th century, which replaced earlier cork balls. The International Table Tennis Federation (ITTF) was founded in 1926, formalizing rules and regulations, leading to its global recognition.</p>
+        </div>
+      </div>
+      <div className="mcontainer4 mcontainer2">
+        
+        <img src="https://assets-global.website-files.com/5ca5fe687e34be0992df1fbe/635bbead37b8c801801315e9_p1914050-jpg-650%C3%97650-.png" width="700px" alt="" />
+        <div className="dis">
+          <h2>Equipment</h2>
+          <li>Table: The playing surface is a rectangular table, typically measuring 9 feet in length, 5 feet in width, and 30 inches in height, with a net dividing it into two equal courts.</li>
+          <li>Racket (Paddle or Bat): Players use a small, light racket made of wood, rubber, and other materials, to hit a small hollow celluloid or plastic ball back and forth across the table.</li>
+          <li>Ball: The ball used in table tennis is lightweight, with a diameter of 40mm for international competitions and 38mm for informal play.
+</li> 
+        </div>
+      </div>
+      <div className="mcontainer5 mcontainer2">
+        
+        <div className="dis">
+          <h2>Gameplay</h2>
+          <li>Singles and Doubles: Table Tennis can be played as singles (one player against another) or doubles (two players on each side). In doubles, players take turns hitting the ball.</li>
+          <li>Serve: The game begins with a serve, where the ball is thrown up from an open palm and struck so that it bounces on the server's side of the table before crossing over the net.</li>
+          <li>Rally: Players take turns hitting the ball back and forth over the net, aiming to score points by making the opponent fail to return the ball within the boundaries of the opponent's side of the table.</li>
+          <li>Scoring: Points are scored when the opponent fails to return the ball, or the ball is hit out of bounds. Matches are typically played to 11 points (though variations exist), and a player must win by a margin of at least two points.</li>
+        </div>
+        
+        <img src="https://www.allabouttabletennis.com/images/doubles-rules.jpg" width="700px" alt="" />
+      </div>
+      <div className="mcontainer6 mcontainer2">
+        
+       
+        
+        <img src="https://theracketlife.com/wp-content/uploads/2022/10/How-Are-Points-Scored-In-Table-Tennis-Doubles-00.jpg" width="700px" alt="" />
+        <div className="dis">
+          <h2>Competitive Scene</h2>
+         <li>Tournaments: Table Tennis has a vibrant competitive scene, with numerous international tournaments organized by the ITTF, including the World Table Tennis Championships, Olympic Games, and World Cup, attracting top players from around the globe.</li>
+         <li>Players: Notable players such as Ma Long, Jan-Ove Waldner, Zhang Jike, and Ding Ning have achieved legendary status in the sport due to their exceptional skills and achievements in international competitions.</li>
+         <li>Paralympic Table Tennis: Table Tennis is also included in the Paralympic Games, with various categories based on players' disabilities, showcasing inclusivity and diversity within the sport.</li>
+        </div>
+      </div>
+      <div className="mcontainer7 mcontainer2">
+        
+       
+        
+        
+        <div className="dis">
+          <h2>Health Benefits</h2>
+          <li>Physical Fitness: Table Tennis is a physically demanding sport that improves cardiovascular health, reflexes, hand-eye coordination, and overall fitness levels.</li>
+          <li>Mental Agility: The fast-paced nature of the game requires quick thinking, strategy, and mental focus, which can improve cognitive functions and concentration.</li>
+          <li>Social Interaction: Table Tennis is often played in social settings, promoting camaraderie, teamwork, and social interaction, contributing to overall well-being and mental health.</li>
+         </div>
+         <img src="https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/607d1495bb63b1001da2a172.png" width="700px" alt="" />
+      </div>
+      <div className="mcontainer8 mcontainer1">
+        <h3 className="text-6xl">Conclusion</h3>
+        <h2>Table Tennis is a dynamic and engaging sport enjoyed by millions worldwide, combining athleticism, strategy, and skill in a compact and accessible format. Its rich history, competitive spirit, and numerous health benefits continue to attract enthusiasts of all ages, making it one of the most beloved indoor sports globally. Whether played casually among friends or competitively on the international stage, table tennis remains a testament to the enduring appeal of sportsmanship and athleticism.</h2>
+      </div>
+      
       
     </div>
   );
