d0a870783dd41c8563ac8a7b7d6547e79ce0ab09
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-05-05 10:03:17 +0530

    update13

--- src/pages/Itsme/Itsme.jsx
+++ src/pages/Itsme/Itsme.jsx
@@ -4,32 +4,34 @@ import './itsme.css';
 const Itsme = () => {
   // Create an array of objects, each containing information about the image
   const imagesData = [
-    { name: 'Akeesha Samaraweera', school: "Anula Vidyalaya, Nugegoda",src: './images/pic3.jpeg' },
-    {name: 'Hirundi Wickramasinghe', school: "Anula Vidyalaya - Nugegoda",src: './images/pic5.jpeg' },
-    {name: 'Lisara Wickramasinghe', school: "Gateway Colombo",src: './images/pic10 (1).jpeg' },
-    {name: 'K.Aathigesh', school: "Hindu College Colombo",src: './images/pic10 (2).jpeg' },
-     {name: 'T Luxshitaa ', school: "Bishop’s College - Colombo",src: './images/pic4.jpeg' },
-    {name: 'Charitha Gimhan de silva', src: './images/DSC_0237.jpg' },
-    {name: 'Shinura kevin', src: './images/DSC_0238 (1).jpg' },
-    {name: 'Sachitha', src: './images/DSC_0243.jpg' },
-    {name: 'Sandew Hulangamuwa', src: './images/DSC_0244.jpg' },
-    {name: 'Janith', src: './images/DSC_0242.jpg' },
-   
-    {name: 'Senul Rathnayaka', src: './images/DSC_0241.jpg' },
-    {name: 'T. Hasira dinusara',src: './images/DSC_0240.jpg' },
-    {name: 'Radinsa & Rohansa',  school: "Gateway College Colombo",src: './images/pic6.jpeg' },
-    {name: 'Methuli Nelinya',src: './images/pic7.jpeg' },
-    {name: 'Thinuli Thiyansa',src: './images/pic8.jpeg' },
-    {name: 'Sandiw Kuruwita',  school: "Royal College, Colombo",src: './images/pic9.jpeg' },
+    { name: 'Risali Perera', school: "Dharmapala Vidyalaya - Pannipitiya", src: './images/pic12.jpg' },
+    { name: 'Minuli Wickramnayake', school: "Musaeus College - Colombo", src: './images/pic11.jpeg' },
+    { name: 'Akeesha Samaraweera', school: "Anula Vidyalaya, Nugegoda", src: './images/pic3.jpeg' },
+    { name: 'Hirundi Wickramasinghe', school: "Anula Vidyalaya - Nugegoda", src: './images/pic5.jpeg' },
+    { name: 'Lisara Wickramasinghe', school: "Gateway Colombo", src: './images/pic10 (1).jpeg' },
+    { name: 'K.Aathigesh', school: "Hindu College Colombo", src: './images/pic10 (2).jpeg' },
+    { name: 'T Luxshitaa ', school: "Bishop’s College - Colombo", src: './images/pic4.jpeg' },
+    { name: 'Charitha Gimhan de silva', src: './images/DSC_0237.jpg' },
+    { name: 'Shinura kevin', src: './images/DSC_0238 (1).jpg' },
+    { name: 'Sachitha', src: './images/DSC_0243.jpg' },
+    { name: 'Sandew Hulangamuwa', src: './images/DSC_0244.jpg' },
+    { name: 'Janith', src: './images/DSC_0242.jpg' },
+
+    { name: 'Senul Rathnayaka', src: './images/DSC_0241.jpg' },
+    { name: 'T. Hasira dinusara', src: './images/DSC_0240.jpg' },
+    { name: 'Radinsa & Rohansa', school: "Gateway College Colombo", src: './images/pic6.jpeg' },
+    { name: 'Methuli Nelinya', src: './images/pic7.jpeg' },
+    { name: 'Thinuli Thiyansa', src: './images/pic8.jpeg' },
+    { name: 'Sandiw Kuruwita', school: "Royal College, Colombo", src: './images/pic9.jpeg' },
     { name: 'Pramukshi  Fernando', school: "Bishop 's College Colombo", src: './images/pic1.jpeg' },
     { name: 'Shaneli Wickramanayaka', school: "Buddhist Ladies Collage - Colombo 7", src: './images/pic2.jpeg' },
-  
-    {src: './images/download.png' },
-    {src: './images/download.png' },
-    {src: './images/download.png' },
-    {src: './images/download.png' },
-  
-   
+
+    { src: './images/download.png' },
+    { src: './images/download.png' },
+    { src: './images/download.png' },
+    { src: './images/download.png' },
+
+
     // Add more objects for each image
   ];
 
