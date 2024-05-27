6889c84110ca0a9a8d2cc12a0056ed9aa8390265
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-05-27 13:23:13 +0530

    werwerwekjh

--- src/components/Matchplayers/DoubleMatchCard.jsx
+++ src/components/Matchplayers/DoubleMatchCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
 import React from 'react';
 
 const DoubleMatchCard = ({ match }) => {
@@ -16,46 +16,59 @@ const DoubleMatchCard = ({ match }) => {
     }
 
     return (
-        <Card>
+        <Card sx={{ margin: 2, padding: 2 }}>
             <CardContent>
-                <Typography variant="h6" component="div" className="text-center">
+                <Typography variant="h6" component="div" align="center">
                     {match.title}
                 </Typography>
-                <Typography variant="body2" color="textSecondary" className="text-center mb-2">
+                <Typography variant="body2" color="textSecondary" align="center" gutterBottom>
                     {match.venue}
                 </Typography>
-                <div className="flex justify-between items-center my-2">
-                    {/* Pair 1 */}
-                    <div className="flex items-center space-x-2">
-                        {match.pair1.map((player, index) => (
-                            <div key={index} className="flex items-center space-x-2">
-                                <img src={player.avatar} alt={player.name} className="w-8 h-8" />
-                                <Typography variant="body2" className="text-xs">
-                                    {player.name}
-                                </Typography>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="text-center">
-                        <Typography variant="body1" className="text-xs">
-                            {match.scores}
-                        </Typography>
-                        <Typography variant="paragraph" color="textSecondary" className="text-xs">
-                            {match.details}
-                        </Typography>
-                    </div>
-                    {/* Pair 2 */}
-                    <div className="flex items-center space-x-2">
-                        {match.pair2.map((player, index) => (
-                            <div key={index} className="flex items-center space-x-2">
-                                <Typography variant="body2" className="text-xs">
-                                    {player.name}
-                                </Typography>
-                                <img src={player.avatar} alt={player.name} className="w-8 h-8" />
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                <Box my={2}>
+                    <Grid container justifyContent="space-between" alignItems="center">
+                        {/* Team Flags and Score */}
+                        <Grid item xs={5} container alignItems="center">
+                            <img src={match.pair1[0].avatar} alt={match.pair1[0].country} style={{ width: 32, height: 32, marginRight: 8 }} />
+                            <img src={match.pair1[1].avatar} alt={match.pair1[1].country} style={{ width: 32, height: 32 }} />
+                        </Grid>
+                        <Grid item xs={2} container direction="column" alignItems="center">
+                            <Typography variant="body1">
+                                {match.scores}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={5} container alignItems="center" justifyContent="flex-end">
+                            <img src={match.pair2[0].avatar} alt={match.pair2[0].country} style={{ width: 32, height: 32, marginRight: 8 }} />
+                            <img src={match.pair2[1].avatar} alt={match.pair2[1].country} style={{ width: 32, height: 32 }} />
+                        </Grid>
+                    </Grid>
+                    <Grid container justifyContent="space-between" alignItems="center" mt={2}>
+                        {/* Pair 1 */}
+                        <Grid item xs={5} container spacing={1} alignItems="center">
+                            {match.pair1.map((player, index) => (
+                                <Grid key={index} item xs={6} container alignItems="center">
+                                    <Typography variant="body2">
+                                        {player.name}
+                                    </Typography>
+                                </Grid>
+                            ))}
+                        </Grid>
+                        <Grid item xs={2} container direction="column" alignItems="center">
+                            <Typography variant="body2" color="textSecondary">
+                                {match.details}
+                            </Typography>
+                        </Grid>
+                        {/* Pair 2 */}
+                        <Grid item xs={5} container spacing={1} alignItems="center" justifyContent="flex-end">
+                            {match.pair2.map((player, index) => (
+                                <Grid key={index} item xs={6} container alignItems="center" justifyContent="flex-end">
+                                    <Typography variant="body2">
+                                        {player.name}
+                                    </Typography>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Grid>
+                </Box>
             </CardContent>
         </Card>
     );
